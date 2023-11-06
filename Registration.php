b0ca0be6adeabfe9fb0501b60cd4c3652dc085e4
TheeLuke <51451143+TheeLuke@users.noreply.github.com>
2023-11-05 18:30:01 -0600

    account creation php and sql

--- Registration.php
+++ Registration.php
@@ -14,7 +14,6 @@
                 reader.readAsDataURL(event.target.files[0]);
             };
         </script>
-    
         <?php
             $servername = "localhost";
             $username = "root";
@@ -28,7 +27,7 @@
             if ($conn->connect_error) {
             die("Connection failed: " . $conn->connect_error);
             }
-            echo "Connected successfully";
+            echo "Connection Successful! YAY";
         ?>
     </head>
     <body>
@@ -69,96 +68,116 @@
             <h1 class="text-center">Service Account Registration Page</h1>
           </div>
           <!-- input first and last name -->
+        <form id="regForm" action="POST">
         <div class="container-sm">
         <div class="mt-3">
             <div class="input-group">
             <span class="input-group-text">First and last name</span>
-            <input type="text" aria-label="First name" class="form-control">
-            <input type="text" aria-label="Last name" class="form-control">
+            <input type="text" aria-label="First name" class="form-control" id="inputFirst">
+            <input type="text" aria-label="Last name" class="form-control" id="inputLast">
           </div>
         </div>
         <div class="container-sm align-content-center">
             <!--  -->
-        <div class="row">
-            <div class="col">
-          <div class="my-3">
-            <label for="InputEmail" class="form-label">Email address</label>
-            <input type="email" class="form-control" id="InputEmail" aria-describedby="emailHelp">
-            <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
-          </div>
-          <div class="mb-3">
-            <label for="InputPassword" class="form-label">Password</label>
-            <input type="password" class="form-control" id="InputPassword">
-          </div>
-          <div class="my-3">
-            <label for="inputState" class="form-label">User's State</label>
-            <select id="inputState" class="form-select">
-                <option selected>Choose...</option>
-                <option value="AL">Alabama</option>
-                <option value="AK">Alaska</option>
-                <option value="AZ">Arizona</option>
-                <option value="AR">Arkansas</option>
-                <option value="CA">California</option>
-                <option value="CO">Colorado</option>
-                <option value="CT">Connecticut</option>
-                <option value="DE">Delaware</option>
-                <option value="DC">District Of Columbia</option>
-                <option value="FL">Florida</option>
-                <option value="GA">Georgia</option>
-                <option value="HI">Hawaii</option>
-                <option value="ID">Idaho</option>
-                <option value="IL">Illinois</option>
-                <option value="IN">Indiana</option>
-                <option value="IA">Iowa</option>
-                <option value="KS">Kansas</option>
-                <option value="KY">Kentucky</option>
-                <option value="LA">Louisiana</option>
-                <option value="ME">Maine</option>
-                <option value="MD">Maryland</option>
-                <option value="MA">Massachusetts</option>
-                <option value="MI">Michigan</option>
-                <option value="MN">Minnesota</option>
-                <option value="MS">Mississippi</option>
-                <option value="MO">Missouri</option>
-                <option value="MT">Montana</option>
-                <option value="NE">Nebraska</option>
-                <option value="NV">Nevada</option>
-                <option value="NH">New Hampshire</option>
-                <option value="NJ">New Jersey</option>
-                <option value="NM">New Mexico</option>
-                <option value="NY">New York</option>
-                <option value="NC">North Carolina</option>
-                <option value="ND">North Dakota</option>
-                <option value="OH">Ohio</option>
-                <option value="OK">Oklahoma</option>
-                <option value="OR">Oregon</option>
-                <option value="PA">Pennsylvania</option>
-                <option value="RI">Rhode Island</option>
-                <option value="SC">South Carolina</option>
-                <option value="SD">South Dakota</option>
-                <option value="TN">Tennessee</option>
-                <option value="TX">Texas</option>
-                <option value="UT">Utah</option>
-                <option value="VT">Vermont</option>
-                <option value="VA">Virginia</option>
-                <option value="WA">Washington</option>
-                <option value="WV">West Virginia</option>
-                <option value="WI">Wisconsin</option>
-                <option value="WY">Wyoming</option>
-            </select>
-        </div>
-        </div>
-        <div class="col my-3">
-            <div class="d-inline-block">
-                <label class="form-label" for="inputFile">Upload Profile Picture</label>
+
+            <div class="row">
+                <div class="col">
+            <div class="my-3">
+                <label for="inputEmail" class="form-label">Email address</label>
+                <input type="email" class="form-control" id="inputEmail" aria-describedby="emailHelp">
+                <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
             </div>
-            <div class="input-group mb-3">
-                <input type="file" class="form-control" accept="image/*" onchange="loadFile(event)">
-              </div>
-              <img src="..." class="img-thumbnail-my-1" alt="preview of uploaded image" id="imagePreview">
-        </div>
-        </div>
-        </div>
-            <button type="submit" class="btn btn-primary">Create Account</button>
+            <div class="mb-3">
+                <label for="inputPassword" class="form-label">Password</label>
+                <input type="password" class="form-control" id="inputPassword">
+            </div>
+            <div class="my-3">
+                <label for="inputState" class="form-label">User's State</label>
+                <select id="inputState" class="form-select">
+                    <option selected>Choose...</option>
+                    <option value="AL">Alabama</option>
+                    <option value="AK">Alaska</option>
+                    <option value="AZ">Arizona</option>
+                    <option value="AR">Arkansas</option>
+                    <option value="CA">California</option>
+                    <option value="CO">Colorado</option>
+                    <option value="CT">Connecticut</option>
+                    <option value="DE">Delaware</option>
+                    <option value="DC">District Of Columbia</option>
+                    <option value="FL">Florida</option>
+                    <option value="GA">Georgia</option>
+                    <option value="HI">Hawaii</option>
+                    <option value="ID">Idaho</option>
+                    <option value="IL">Illinois</option>
+                    <option value="IN">Indiana</option>
+                    <option value="IA">Iowa</option>
+                    <option value="KS">Kansas</option>
+                    <option value="KY">Kentucky</option>
+                    <option value="LA">Louisiana</option>
+                    <option value="ME">Maine</option>
+                    <option value="MD">Maryland</option>
+                    <option value="MA">Massachusetts</option>
+                    <option value="MI">Michigan</option>
+                    <option value="MN">Minnesota</option>
+                    <option value="MS">Mississippi</option>
+                    <option value="MO">Missouri</option>
+                    <option value="MT">Montana</option>
+                    <option value="NE">Nebraska</option>
+                    <option value="NV">Nevada</option>
+                    <option value="NH">New Hampshire</option>
+                    <option value="NJ">New Jersey</option>
+                    <option value="NM">New Mexico</option>
+                    <option value="NY">New York</option>
+                    <option value="NC">North Carolina</option>
+                    <option value="ND">North Dakota</option>
+                    <option value="OH">Ohio</option>
+                    <option value="OK">Oklahoma</option>
+                    <option value="OR">Oregon</option>
+                    <option value="PA">Pennsylvania</option>
+                    <option value="RI">Rhode Island</option>
+                    <option value="SC">South Carolina</option>
+                    <option value="SD">South Dakota</option>
+                    <option value="TN">Tennessee</option>
+                    <option value="TX">Texas</option>
+                    <option value="UT">Utah</option>
+                    <option value="VT">Vermont</option>
+                    <option value="VA">Virginia</option>
+                    <option value="WA">Washington</option>
+                    <option value="WV">West Virginia</option>
+                    <option value="WI">Wisconsin</option>
+                    <option value="WY">Wyoming</option>
+                </select>
+            </div>
+            </div>
+            <div class="col my-3">
+                <div class="d-inline-block">
+                    <label class="form-label" for="inputFile">Upload Profile Picture</label>
+                </div>
+                <div class="input-group mb-3">
+                    <input type="file" class="form-control" accept="image/*" onchange="loadFile(event)">
+                </div>
+                <img src="..." class="img-thumbnail-my-1" alt="preview of uploaded image" id="imagePreview">
+            </div>
+            </div>
+            </div>
+                <button type="submit" class="btn btn-primary">Create Account</button>
+        </form>
+        <?php
+
+            $firstName = $_POST["inputFirst"];
+            $lastName = $_POST["inputLast"];
+            $username = $_POST["inputEmail"];
+            $password = $_POST["inputPassword"];
+            $state = $_POST["inputState"];
+            
+            $sql = "INSERT INTO user (Username, Password, FirstName, LastName, State) VALUES ('$Username', '$password', '$firstName', '$lastName', '$state')"
+            if ($conn->query($sql) === TRUE) {
+                echo "User registered successfully";
+            } else {
+                echo "Error: " . $sql . "<br>" . $conn->error;
+            }
+
+            $conn->close();
+        ?>
     </body>
 </html>
