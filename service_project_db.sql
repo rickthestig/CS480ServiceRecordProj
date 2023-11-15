e37e686b9a9f34b0259f7d616d8110a2c76418f7
TheeLuke <51451143+TheeLuke@users.noreply.github.com>
2023-11-15 08:44:10 -0600

    Update service_project_db.sql

--- service_project_db.sql
+++ service_project_db.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 01, 2023 at 10:44 PM
--- Server version: 10.4.27-MariaDB
--- PHP Version: 8.2.0
+-- Generation Time: Nov 15, 2023 at 03:42 PM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -42,6 +42,14 @@ CREATE TABLE `service` (
   `EndTime` time NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Dumping data for table `service`
+--
+
+INSERT INTO `service` (`ServiceID`, `Name`, `Blurb`, `Description`, `Organizer`, `Location`, `Rating`, `MaxUserCount`, `StartDate`, `StartTime`, `EndDate`, `EndTime`) VALUES
+(1, 'Dane County', 'service project', 'big service project in dane county', 'organizer', 'location', 5, 10, '2023-11-03', '20:35:36', '2023-11-11', '21:35:36'),
+(2, 'Test Service', 'this is a test blurb', 'this is a test description', 'Tester Test', 'Testington', 3, 5, '2016-11-22', '12:38:28', '2023-09-22', '04:15:42');
+
 -- --------------------------------------------------------
 
 --
@@ -50,13 +58,36 @@ CREATE TABLE `service` (
 
 CREATE TABLE `user` (
   `UserID` int(11) NOT NULL,
-  `Username` varchar(25) NOT NULL,
   `Password` varchar(25) NOT NULL,
   `FirstName` varchar(25) NOT NULL,
   `LastName` varchar(25) NOT NULL,
-  `Picture` varchar(25) DEFAULT NULL,
-  `State` varchar(25) DEFAULT NULL
+  `Picture` varchar(300) DEFAULT NULL,
+  `State` varchar(25) DEFAULT NULL,
+  `Username` varchar(300) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`UserID`, `Password`, `FirstName`, `LastName`, `Picture`, `State`, `Username`) VALUES
+(5, '', '', '', NULL, '', ''),
+(6, '', '', '', NULL, '', ''),
+(7, '', '', '', NULL, '', ''),
+(8, '', '', '', NULL, '', ''),
+(9, '', '', '', NULL, '', ''),
+(10, '', '', '', NULL, '', ''),
+(11, '', '', '', NULL, '', ''),
+(12, '', '', '', NULL, '', ''),
+(13, '', '', '', NULL, '', ''),
+(14, '', '', '', NULL, '', ''),
+(15, '', '', '', NULL, '', ''),
+(16, '', '', '', NULL, '', ''),
+(17, '', '', '', NULL, '', ''),
+(18, '', '', '', NULL, '', ''),
+(19, '', '', '', NULL, '', ''),
+(21, 'asdasd', 'asdas', 'asdasd', NULL, 'CA', 'lsullivan@edgewood.edu'),
+(22, 'testpassword', 'test_first_name', 'test_last_name', NULL, NULL, 'testAccount');
 
 -- --------------------------------------------------------
 
@@ -100,13 +131,13 @@ ALTER TABLE `userprojects`
 -- AUTO_INCREMENT for table `service`
 --
 ALTER TABLE `service`
-  MODIFY `ServiceID` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `ServiceID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `UserID` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `UserID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
 
 --
 -- Constraints for dumped tables
