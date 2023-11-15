b54937d59a3b5b3c87820d281df324b8de2b9284
rickthestig <erekg13@gmail.com>
2023-11-15 16:24:45 -0600

    redo button so that form will post correctly

--- Searcher.php
+++ Searcher.php
@@ -39,7 +39,7 @@
             $ID = $result->fetch_assoc();
             $ID = $ID["ServiceID"];
             echo "<form action=\"ServicePage.php\" method=\"post\">";
-            echo "<input type=\"text\" id=\"val\" value=\"$ID\" disabled>";
+            echo "<input type=\"text\" id=\"val\" value=\"$ID\" readonly=\"readonly\">";
             echo "<input type=\"submit\">";
             /* the chance this acutally works is basically none, good luck my small amount of remaining sanity */
             /* I HATE ARRAYS */
