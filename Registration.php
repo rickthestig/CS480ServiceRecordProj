82c82b8759275868c2fca8e00e7ffdc111d6081b
TheeLuke <51451143+TheeLuke@users.noreply.github.com>
2023-12-03 16:44:45 -0600

    Removed navbar on signin & signup

--- Registration.php
+++ Registration.php
@@ -34,61 +34,6 @@ session_start();
         ?>
     </head>
     <body>
-        <!-- navbar -->
-        <nav class="navbar navbar-expand-lg bg-body-tertiary">
-            <span class="navbar-brand">Service Project</span>
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav">
-                    <li class="nav-item active">
-                        <a href="BrowseServiceProjs.php" class="nav-link">Home</a>
-                    </li>
-                </ul>
-                <div class="container-sm align-content-center">
-                    <form class="d-flex container-fluid justify-content-start" role="search" method="post" action="BrowseServiceProjs.php">
-                        <input class="form-control me-2" id="searchInput" name="searchInput" type="search" placeholder="Search" aria-label="Search">
-                        <input type="submit" class="btn btn-outline-success" name="searchBtn" id="searchBtn" value="Search" >
-                    <a href="AdvancedSearch.php">
-                        <input type="button" class="btn btn-sm btn-outline-secondary" name="advancedBtn" id="advancedBtn" value="Advanced">
-                        </a>
-                    </form>
-                    <?php
-
-                        $search = isset($_POST["searchInput"]) ? $_POST["searchInput"] : "";
-
-                        if(isset($_POST["searchBtn"])) {
-                            $sql = "SELECT Name FROM Service WHERE Name LIKE '%$search%'";
-                            $result = $conn->query($sql);
-
-
-                            //will have to query this on BrowseServerProjs.php - form redirects them on 'post'
-                        }
-
-
-                    ?>
-                </div>
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDarkDropdown" aria-controls="navbarNavDarkDropdown" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                    </button>
-                    <div class="justify-content-end collapse navbar-collapse" id="navbarNavDarkDropdown">
-                    <ul class="navbar-nav">
-                        <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" id="navbarDarkDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        </a>
-                        <ul class="dropdown-menu dropdown-menu-dark dropdown-menu-lg-end" aria-labelledby="navbarDarkDropdownMenuLink">
-                            <li><a class="dropdown-item" href="Settings.php">Settings</a></li>
-                            <li><a class="dropdown-item" href="#">Sign out</a></li>
-                        </ul>
-                        </li>
-                    </ul>
-                </div>
-                <div class="d-flex justify-content-end">
-                    <a href="Settings.php">
-                        <img class="d-inline-block" src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/7e/Circle-icons-profile.svg/1200px-Circle-icons-profile.svg.png"
-                        width="30" height=30>
-                    </a>
-                </div>
-            </div>
-        </nav>
           <div class="mt-3">
             <h1 class="text-center">Service Account Registration Page</h1>
           </div>
